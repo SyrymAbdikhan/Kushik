5731294f0fcbecff80d17eb1c66440e0430120e9
SyrymAbdikhan <syrym.abdikhan@gmail.com>
2025-03-21 01:16:14 +0100

    initial commit

---
+++ src/main/java/dev/shetel/kushik/KushikApplication.java
@@ -0,0 +1,13 @@
+package dev.shetel.kushik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KushikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KushikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/shetel/kushik/KushikApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.shetel.kushik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KushikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
